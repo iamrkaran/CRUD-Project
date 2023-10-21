f82f6090fcde4093394688069f7b2af7726edabd
Topbard <108051661+TOPBARD@users.noreply.github.com>
2023-10-21 18:29:47 +0000

    Added error handling on backend and modified model

--- server/modles/Students.js
+++ server/modles/Students.js
@@ -1,8 +1,21 @@
-const mongoose=require('mongoose')
-const studentSchema= mongoose.Schema({
-    _id:mongoose.Schema.Types.ObjectId,
-    firstname:{type:String,require:true},
-    lastname:{type:String,require:true},
-    place:{type:String,require:true},
+const mongoose = require('mongoose');
+
+const studentSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  place: {
+    type: String,
+    required: true,
+  },
 });
-module.exports=mongoose.model('Student',studentSchema);
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
